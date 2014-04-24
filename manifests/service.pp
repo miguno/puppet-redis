d6f3b17c2107d4966afa9affbd0f787bdadced18
Michael G. Noll <mnoll@verisign.com>
2014-04-24 12:08:58 +0200

    Simplify require statement

--- manifests/service.pp
+++ manifests/service.pp
@@ -25,7 +25,7 @@ class redis::service inherits redis {
         stdout_logfile_keep    => $service_stdout_logfile_keep,
         stderr_logfile_maxsize => $service_stderr_logfile_maxsize,
         stderr_logfile_keep    => $service_stderr_logfile_keep,
-        require                => [ Class['redis::install'], Class['redis::config'], Class['::supervisor'] ],
+        require                => [ Class['redis::config'], Class['::supervisor'] ],
     }
 
     if $service_enable == true {
